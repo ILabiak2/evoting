c0decf4d1179362059a384bcd8d179d898150501
Ivan Labiak <71697480+ILabiak@users.noreply.github.com>
2025-06-15 20:33:41 +0300

    add sidebar It still need to be refactored to use theme styling

--- client/app/dashboard/page.jsx
+++ client/app/dashboard/page.jsx
@@ -3,6 +3,7 @@ import React, { useState, } from 'react';
 import ProtectedRoute from '@/app/context/ProtectedRoute';
 import SignupForm from '@/components/signup-form';
 import { Navbar } from '@/components/navbar';
+import Sidebar from '@/components/app-sidebar'
 import { cn } from "@/lib/utils";
 
 
@@ -11,7 +12,8 @@ export default function Home() {
     return (
         <ProtectedRoute>
             <div>
-                <Navbar />
+                {/* <Navbar /> */}
+                <Sidebar />
             </div>
         </ProtectedRoute>
     );
